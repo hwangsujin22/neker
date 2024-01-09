44449faf1fea17e40c56d96633639e444e2a1402
wlgusq <ziih2521@gmail.com>
2024-01-09 18:39:12 +0900

    2024.01.09 6:40pm 업데이트

--- script/slide.js
+++ script/slide.js
@@ -4,8 +4,6 @@
 // 1. 변수선언
 const slide = document.querySelector('.slide'); //배너 이미지 목록의 부모요소
 const slide_img = document.querySelectorAll('.slide li') //이미지목록
-const l_btn = document.getElementById('l_btn'); //이전버튼
-const r_btn = document.getElementById('r_btn'); //다음버튼
 
 const img_n = slide_img.length; //li개수를 변수에 담음. 총3개
 // console.log(img_n); // 3
@@ -26,25 +24,6 @@ function mslide(n){
   console.log(slide.style.left);
 }
 
-// 이전버튼 클릭시 왼쪽으로 슬라이드 이동
-l_btn.addEventListener('click', function(){
-  if(count>0){ //만약에 카운트 값이 0보다 크면
-    mslide(count-1); //카운트 값에서 1을 빼서 mslide에 넘김
-  }else{
-    mslide(img_n-s_count); //li개수 -1
-  }
-});
-
-// 다음버튼 클릭시 오른쪽으로 슬라이드 이동
-r_btn.addEventListener('click', function(){
-  if(count < img_n-s_count){ //0보다 li개수가 많다면
-    mslide(count+1); //0+1해서 넘김
-  }else{ //그렇지 않다면
-    mslide(0); //0을 대입해서 처음으로 돌아가게 함
-  }
-  // 1, 2, 0순으로 반복
-});
-
 // 3초마다 자동으로 슬라이드 움직이게 하기
 // 시간객체 = setInterval (함수명, 반복시간)
 
@@ -57,6 +36,7 @@ let Timer = setInterval(function(){
   }
   mslide(count); //mslide에 count값을 넘겨서 자동으로 움직이게 한다.
 }, 3000);
+
 
 //---------------------------------------------------------------------------
 
